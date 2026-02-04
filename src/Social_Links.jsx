ff31367c078dc9da96f00270d955ad0cc15f5272
mc489 <146339365+mc489@users.noreply.github.com>
2026-02-05 00:20:35 +0800

    optimized code for social links update

--- src/Social_Links.jsx
+++ src/Social_Links.jsx
@@ -6,179 +6,74 @@ import { FaBehance } from 'react-icons/fa';
 import { FaYoutube } from 'react-icons/fa'; // 'fa' for Font Awesome
 import { useMediaQuery } from 'react-responsive';
 import { LuLink } from "react-icons/lu";
+import { icon } from '@fortawesome/fontawesome-svg-core';
+import { color } from 'framer-motion';
 
 function SocialLinks() {
-const isDesktopOrLaptop = useMediaQuery({
+    const isDesktopOrLaptop = useMediaQuery({
         query: '(min-width: 700px)'
     })
-    const isBigScreen = useMediaQuery({ query: '(min-width: 1824px)' })
     const isTabletOrMobile = useMediaQuery({ query: '(max-width:  699px)' })
-    const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
-    const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' })
+
+
+    const socialLinks = [{ name: 'mc489', icon: FaGithub, color: 'black', url: 'https://github.com/mc489'},
+    { name: 'MC Ganir', icon: FaLinkedin, color: 'black', url: 'https://www.linkedin.com/in/mc-ganir-552264210/'},
+    { name: 'MC Ganir', icon: FaFacebook, color: 'black', url: 'https://www.facebook.com/evmcpov' },
+    { name: 'mc ganir', icon: FaBehance, color: 'black', url: 'https://www.behance.net/mcganir'},
+    { name: 'mcjemuelganir', icon: FaYoutube, color: 'black', url:'https://www.youtube.com/@mcjemuelganir' },
+    ];
+    
+    const renderLinks = (iconSize, textSize) => (
+        socialLinks.map((link, index) => {
+            const Icon = link.icon; 
+            return (
+                <a key={index} href={link.url} target="_blank" rel="noopener noreferrer" className="w-full">
+                    <button className="mb-[4px] cursor-pointer w-full rounded-[12px] border-1 border-gray-300 px-2 py-2 hover:-translate-y-0.5 duration-200 ">
+                        <div className="flex flex-row items-center gap-2">
+                            <Icon size={iconSize} style={{ color: link.color }} />
+                            <span className={`${textSize} text-black`}>{link.name}</span>
+                        </div>
+                    </button>
+                </a>
+            );
+        })
+    );
+
 
     return (
-<>
-        {isDesktopOrLaptop &&
-        <div className="flex  items-center justify-center ">
+        <>
+            {isDesktopOrLaptop &&
+                <div className="flex items-center justify-center ">
 
-            <div className=" hover:shadow-sm flex-col  w-full relative max-w-[250px]  gap-1 rounded-[16px] p-[15px] mt-[10px] text-left border border-gray-300">
-               
-               <div className='flex items-center gap-2 mb-[14px] '>
-                
-                
-              <LuLink size={14} color="black"/>
-                <span className="text-lg font-semibold">Social Links</span>
-</div>
+                    <div className=" hover:shadow-sm flex-col w-full relative max-w-[250px]  gap-1 rounded-[16px] p-[15px] mt-[10px] text-left border border-gray-300">
 
-
-                <a href="https://web.facebook.com/evmcpov" target="_blank" rel="noopener noreferrer">
-                    <button className="mb-[4px] cursor-pointer w-full rounded-[12px] border-1 border-gray-300 px-2 py-2 hover:-translate-y-0.5 duration-200 ">
-                        <div className="items-center flex flex-row items-center gap-2 ">
-
-                            <FaFacebook size={14} color="black" />
-                            <span className='text-[14px]'> MC Ganir</span>
-
+                        <div className='flex items-center gap-2 mb-[10px] '>
+                            <LuLink size={14} color="black" />
+                            <span className="text-lg font-semibold">Social Links</span>
                         </div>
-                    </button>
-                </a>
+                            {renderLinks(14, "text-[14px] ")}
+                       
+                    </div>
+                </div>
+            }
+
+            {isTabletOrMobile &&
+                <div className="flex w-full justify-center items-center  ">
+
+                    <div className=" flex flex-col relative max-w-[330px] gap-1 rounded-[16px] w-full p-[15px] mt-[2px] text-left border border-gray-300 ">
 
 
-                <a href=" https://www.linkedin.com/in/mc-ganir-552264210/" target="_blank" rel="noopener noreferrer">
-                    <button className="mb-[4px] cursor-pointer w-full rounded-[12px] border-1 border-gray-300 px-2 
-                    py-2 hover:-translate-y-0.5  duration-200 ">
-                        <div className="items-center flex flex-row items-center gap-2 ">
 
-                            <FaLinkedin size={14} color="black" />
-                            <span className='text-[14px]'>
+                        <div className='flex mb-2 gap-2 items-center'>
 
-                                MC Ganir
-                            </span>
-                        </div>
-                    </button>
-                </a>
-
-
-                <a href=" https://github.com/mc489" target="_blank" rel="noopener noreferrer">
-                    <button className="mb-[4px] cursor-pointer w-full rounded-[12px] border-1 border-gray-300 px-2 py-2 hover:-translate-y-0.5 duration-200 ">
-                        <div className="items-center flex flex-row items-center gap-2 ">
-                            <FaGithub size={14} color="#181717" />
-                            <span className='text-[14px]'>
-
-                                @mc489     </span>
-                        </div>
-                    </button>
-                </a>
-
-
-                <a href=" https://www.behance.net/mcganir" target="_blank" rel="noopener noreferrer">
-                    <button className="mb-[4px] cursor-pointer w-full rounded-[12px] border-1 border-gray-300 px-2 py-2 hover:-translate-y-0.5 duration-200 ">
-                        <div className="items-center flex flex-row items-center gap-2 ">
-                            <FaBehance size={14} color="black" />
-                            <span className='text-[14px]'>
-                                mc ganir
-                            </span>
-                        </div>
-                    </button>
-                </a>
-
-
-                <a href=" https://www.youtube.com/@mcjemuelganir" target="_blank" rel="noopener noreferrer">
-                    <button className="cursor-pointer w-full rounded-[12px] border-1 border-gray-300 px-2 py-2 hover:-translate-y-0.5 duration-200 ">
-                        <div className="items-center flex flex-row items-center gap-2 ">
-                            <FaYoutube size={14} color="black" />
-                            <span className='text-[14px]'>
-                                mcjemuelganir
-                            </span>
+                            <LuLink size={12} color="black" />
+                            <span className="text-[16px] font-semibold">Social Links</span>
                         </div>
 
-
-                    </button>
-                </a>
-
-            </div>
-        </div>
-        }
-
-          {isTabletOrMobile &&
-        <div className="flex w-full justify-center items-center  ">
-
-            <div className=" flex flex-col relative max-w-[330px] gap-1 rounded-[16px] w-full p-[15px] mt-[2px] text-left border border-gray-300 ">
-
-
-
-                <div className='flex mb-2 gap-2 items-center'>
-                    
- <LuLink size={12} color="black"/>
-                <span className="text-[16px] font-semibold">Social Links</span>
-
-</div>
-
-                <a href="https://web.facebook.com/evmcpov" target="_blank" rel="noopener noreferrer">
-                    <button className="cursor-pointer w-full rounded-[12px] border-1 border-gray-300 px-2 py-2 hover:-translate-y-0.5 duration-200 ">
-                        <div className="items-center flex flex-row items-center gap-2 ">
-
-                            <FaFacebook size={10} color="black" />
-                            <span className='text-[10px]'> MC Ganir</span>
-
-                        </div>
-                    </button>
-                </a>
-
-
-                <a href=" https://www.linkedin.com/in/mc-ganir-552264210/" target="_blank" rel="noopener noreferrer">
-                    <button className="cursor-pointer w-full rounded-[12px] border-1 border-gray-300 px-2 py-2 hover:-translate-y-0.5  duration-200 ">
-                        <div className="items-center flex flex-row items-center gap-2 ">
-
-                            <FaLinkedin size={10} color="black" />
-                            <span className='text-[10px]'>
-
-                                MC Ganir
-                            </span>
-                        </div>
-                    </button>
-                </a>
-
-
-                <a href=" https://github.com/mc489" target="_blank" rel="noopener noreferrer">
-                    <button className="cursor-pointer w-full rounded-[12px] border-1 border-gray-300 px-2 py-2 hover:-translate-y-0.5 duration-200 ">
-                        <div className="items-center flex flex-row items-center gap-2 ">
-                            <FaGithub size={10} color="#181717" />
-                            <span className='text-[10px]'>
-
-                                @mc489     </span>
-                        </div>
-                    </button>
-                </a>
-
-
-                <a href=" https://www.behance.net/mcganir" target="_blank" rel="noopener noreferrer">
-                    <button className="cursor-pointer w-full rounded-[12px] border-1 border-gray-300 px-2 py-2 hover:-translate-y-0.5 duration-200 ">
-                        <div className="items-center flex flex-row items-center gap-2 ">
-                            <FaBehance size={10} color="black" />
-                            <span className='text-[10px]'>
-                                mc ganir
-                            </span>
-                        </div>
-                    </button>
-                </a>
-
-
-                <a href=" https://www.youtube.com/@mcjemuelganir" target="_blank" rel="noopener noreferrer">
-                    <button className="cursor-pointer w-full rounded-[12px] border-1 border-gray-300 px-2 py-2 hover:-translate-y-0.5 duration-200 ">
-                        <div className="items-center flex flex-row items-center gap-2 ">
-                            <FaYoutube size={10} color="black" />
-                            <span className='text-[10px]'>
-                                mcjemuelganir
-                            </span>
-                        </div>
-
-
-                    </button>
-                </a>
-
-            </div>
-        </div>
-        }
+                         {renderLinks(10, "text-[10px] ")}
+                    </div>
+                </div>
+            }
         </>
     )
 }
