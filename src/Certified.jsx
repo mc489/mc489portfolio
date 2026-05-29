3af73646679c7ae661cd51ee667127057f2a634d
mc489 <146339365+mc489@users.noreply.github.com>
2026-05-30 00:19:51 +0800

    update certificaitons

--- src/Certified.jsx
+++ src/Certified.jsx
@@ -1,80 +1,96 @@
-
-
-import { FaLinkedin, FaFacebook, FaGithub, FaBehance, FaYoutube } from 'react-icons/fa';
 import { useMediaQuery } from 'react-responsive';
-import { LuLink, LuBadgeCheck } from "react-icons/lu";
-import Cert from "./assets/uiuxcert.jpg"
+import { LuBadgeCheck } from "react-icons/lu";
+import uiux from "./assets/uiuxcert.jpg";
+import frontend from "./assets/frontendcert.jpg";
+import uiux2 from "./assets/uiuxcert2.png";
+import webdev from "./assets/webdevcert.png";
 
 const Certified = () => {
-
-
     const isDesktopOrLaptop = useMediaQuery({
         query: '(min-width: 700px)'
-    })
-    const isTabletOrMobile = useMediaQuery({ query: '(max-width:  699px)' })
+    });
+    const isTabletOrMobile = useMediaQuery({
+        query: '(max-width: 699px)'
+    });
 
-
-    const certificate = [
-        { name: 'UI/UX for Beginners', url: 'https://simpli-web.app.link/e/EP2TjmeBW2b' },
-        { name: 'ReactJS for Beginners', url: 'https://www.linkedin.com/in/mc-ganir-552264210/' },
-        { name: 'Introduction to Front End Development', url: 'https://www.facebook.com/evmcpov' },
-
+    // 1. Define the data array for the visual cards
+    const certificationsData = [
+        {
+            id: 1,
+            title: "UI/UX Design",
+            url: "https://simpli-web.app.link/e/IuFKoAM5Q2b",
+            image: uiux,
+            alt: "UI/UX Certification"
+        },
+        {
+            id: 2,
+            title: "Frontend Dev",
+            url: "https://simpli-web.app.link/e/tQbmuihXw3b",
+            image: frontend,
+            alt: "Frontend Certification"
+        },
+           {
+            id: 3,
+            title: "UX Design",
+            url: "https://www.testdome.com/certificates/594fcfff6f65426b874b3b7392fa800c",
+            image: uiux2,
+            alt: "UX Certification"
+        },
+        {
+            id: 4,
+            title: "Web Dev",
+            url: "https://www.testdome.com/certificates/b916ac880e3e488daa8603090e01bd6e",
+            image: webdev,
+            alt: "UX Certification"
+        }
     ];
 
-    const renderLinks = (textSize) => (
-        certificate.map((link, index) => {
-            const Icon = link.icon;
-            return (
-                <a key={index} href={link.url} target="_blank" rel="noopener noreferrer" className="w-full">
-                    <button className="mb-[4px] cursor-pointer w-full rounded-[12px] border-1 border-gray-300 px-2 py-2 hover:-translate-y-0.5 duration-200 ">
-                        <div className="flex flex-col text-left gap-2">
-                            <span className={`${textSize}  `}>{link.name}</span>
-                            <span className='text-[12px]' >simplilearn</span>
-                        </div>
-                    </button>
-                </a>
-            );
-        })
-    );
+
 
 
     return (
         <>
             {isDesktopOrLaptop &&
                 <div className="flex items-center justify-center ">
+                    <div className="bg-white dark:bg-black hover:shadow-sm flex-col w-fit gap-1 rounded-[16px] p-[15px] mt-[10px] text-left border border-gray-300">
 
-                    <div className=" bg-white dark:bg-black hover:shadow-sm flex-col w-fit  gap-1 rounded-[16px] p-[15px] mt-[10px] text-left border border-gray-300">
-
-                        <div className=" flex items-center justify-between relative gap-1 rounded-[16px] 
-                         w-full  mt-[5px] gap-2  text-left">
-
-                            <div className='flex items-center gap-2 mb-[10px] '>
+                        <div className="flex items-center justify-between relative rounded-[16px] w-full mt-[5px] gap-2 text-left">
+                            <div className='flex items-center gap-2 mb-[10px]'>
                                 <LuBadgeCheck size={14} />
                                 <span className="text-lg font-semibold">Recent Certification</span>
                             </div>
                         </div>
-                        <a href="https://simpli-web.app.link/e/IuFKoAM5Q2b" target="_blank" rel="noopener noreferrer" className="w-full">
 
-                            <button className="w-full cursor-pointer">
-                                <img
-                                    src={Cert}
-                                    alt="Certification"
-                                    className="w-[578px] h-auto rounded-[12px] object-cover"
-                                />
-                            </button>
-                        </a>
+                        <div className="w-[578px] max-w-full h-auto items-center grid grid-cols-4 gap-4">
 
+                            {/* 2. Map through the certificationsData array */}
+                            {certificationsData.map((cert) => (
+                                <div key={cert.id} className="flex  flex-col gap-2">
+                                    <h3 className="text-sm font-medium text-black dark:text-white ml-1">
+                                        {cert.title}
+                                    </h3>
+                                    <a href={cert.url} target="_blank" rel="noopener noreferrer" className="w-full">
+                                        <button className="w-full  cursor-pointer hover:-translate-y-0.5 duration-200">
+                                            <img
+                                                src={cert.image}
+                                                alt={cert.alt}
+                                                className="w-full  h-[100px] rounded-[12px]"
+                                            />
+                                        </button>
+                                    </a>
+                                </div>
+                            ))}
+
+                        </div>
                     </div>
-                </div >
+                </div>
             }
-
             {
                 isTabletOrMobile &&
                 <div className="flex w-full justify-center items-center gap-2 ">
 
-                    <div className="bg-white dark:bg-black flex flex-col relative max-w-[330px] gap-1 rounded-[16px] w-full p-[15px]  mt-[10px]  text-left border border-gray-300 ">
-
-
+                    <div className="bg-white dark:bg-black flex flex-col relative 
+                    max-w-[330px] gap-1 rounded-[16px] w-full p-[15px]  mt-[10px]  text-left border border-gray-300 ">
 
 
                         <div className='flex mb-2 gap-2 items-center'>
@@ -82,16 +98,31 @@ const Certified = () => {
                             <LuBadgeCheck size={12} />
                             <span className="text-[16px] font-semibold">Recent Certification</span>
                         </div>
-                        <a href="https://simpli-web.app.link/e/IuFKoAM5Q2b" target="_blank" rel="noopener noreferrer" className="w-full">
 
-                            <button className="w-full cursor-pointer">
-                                <img
-                                    src={Cert}
-                                    alt="Certification"
-                                    className="w-full h-auto rounded-[12px] object-cover"
-                                />
-                            </button>
-                        </a>
+
+
+
+                        <div className="w-[578px] max-w-full h-auto gap-4">
+
+                            {/* 2. Map through the certificationsData array */}
+                            {certificationsData.map((cert) => (
+                                <div key={cert.id} className="flex flex-col gap-2">
+                                    <h3 className="text-sm font-medium text-black dark:text-white ml-1">
+                                        {cert.title}
+                                    </h3>
+                                    <a href={cert.url} target="_blank" rel="noopener noreferrer" className="w-full">
+                                        <button className="w-full cursor-pointer hover:-translate-y-0.5 duration-200">
+                                            <img
+                                                src={cert.image}
+                                                alt={cert.alt}
+                                                className="w-full rounded-[12px]"
+                                            />
+                                        </button>
+                                    </a>
+                                </div>
+                            ))}
+
+                        </div>
 
                     </div>
                 </div>
